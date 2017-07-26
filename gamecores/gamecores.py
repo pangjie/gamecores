dd43ea875fafbb2ae855e4a183dacb11e67a171b
Jie Pang <pangjiefb@gmail.com>
2017-07-26 18:36:50 -0400

    regular commit

--- gamecores/gamecores.py
+++ gamecores/gamecores.py
@@ -5,7 +5,7 @@
 import os
 
 from difflib import SequenceMatcher
-from time import time, sleep
+from time import time, sleep, mktime, strptime
 import re
 
 from shortuuid import uuid
@@ -82,6 +82,9 @@ def calupdate(deep):
         tba_data_patch = {'type': 'tba', 'src_id': i['id']}
         [j.update(schedualed_data_patch) for j in sch_list]
         [j.update(tba_data_patch) for j in tba_list]
+        # Insert ID for each data
+        [j.update({'id': uuid()}) for j in sch_list]
+        [j.update({'id': uuid()}) for j in tba_list]
         # Insert new data to db.
         db.insert_multiple(sch_list)
         db.insert_multiple(tba_list)
@@ -95,12 +98,28 @@ def calupdate(deep):
 
 
 @cal.command()
-@click.option('--title', default='')
-@click.option('--platform', default='')
-def calsearch(title, platform):
+@click.option('--title', default='---')
+@click.option('--platform', '-p', default='---')
+@click.option('--year', '-y', default='---')
+@click.option('--timesection', '-t', nargs=2, default=(0, 0), type=int)
+@click.option('--alldata', default=False, is_flag=True)
+def calsearch(title, platform, year, timesection, alldata):
     """Show information in the database"""
     title_set = set()
     pf_set = set()
+    year_set = set()
+    time_set = set()
+    final_set = set()
+    search_set_list = [title_set, pf_set, year_set, time_set]
+
+    # Return All Data
+    if not alldata:
+        alldata_list = []
+        alldata_list += db.search(Q.type == 'sched')
+        alldata_list += db.search(Q.type == 'tba')
+        for i in alldata_list:
+            print i
+        return alldata_list
 
     # Search Title
     def tt(val, tt):
@@ -109,32 +128,60 @@ def calsearch(title, platform):
         similarity = SequenceMatcher(None, val, tt).ratio() > 0.6
         superset = set(re.split(r': | ', val)).issuperset(tt.split(' '))
         subset = set(re.split(r': | ', val)).issubset(tt.split(' '))
-        if similarity or superset or subset:
-            return True
-        return False
+        return similarity or superset or subset
     [title_set.add(i['title']) for i in db.search(Q.title.test(tt, title))]
 
     # Search Platform
     def pf(val, pf):
-        # print list(pf)
         return bool(set(val).issuperset(pf.split('&')))
     for i in platform.split('|'):
         [pf_set.add(j['title']) for j in db.search(Q.platform.test(pf, i))]
 
-    for i in xrange(2008, 2019):
-        print i
-        x = len(db.search(where('year') == str(i)))
-        if x < 3:
-            print db.search(where('year') == str(i))
-        else:
-            print x
-    return
+    # Search Year
+    [year_set.add(i['title']) for i in db.search(Q.year == year)]
+
+    # Search Time Zone
+    try:
+        tz_start = mktime(strptime(str(timesection[0]), "%Y%m%d"))
+        tz_stop = mktime(strptime(str(timesection[1]), "%Y%m%d"))
+    except ValueError:
+        tz_start = 0
+        tz_stop = 0
+
+    def tz(val, timesection):
+        return True if tz_start <= val <= tz_stop else False
+    timesection_result = db.search(Q.rls_ts.test(tz, timesection))
+    [time_set.add(i['title']) for i in timesection_result]
+
+    # Final Set
+    final_set = title_set | pf_set | year_set | time_set
+    for i in search_set_list:
+        if i != set():
+            final_set = final_set & i
+    print final_set
+    for i in final_set:
+        xx = db.search(Q.title == i)
+        for xxx in xx:
+            print xxx
+    return final_set
 
 
 @cal.command()
 def calexport():
     """Export calendar data to a ics file."""
     print("tring to generate an ics file")
+    return
+
+
+@cal.command()
+def calstat():
+    """Satisfaction Section"""
+    total = 0
+    for i in xrange(2008, 2019):
+        sum_year = len(db.search(Q.year == str(i)))
+        print('Year: %s  Sum: %s' % (i, sum_year))
+        total += sum_year
+    print('Statistic: %s' % total)
     return
 
 
@@ -146,3 +193,4 @@ if __name__ == '__main__':
     # calinit()
     # calupdate()
     calsearch()
+    # calstat()
